62d8a08a527cfc51e9e852e489bbe2ff6735e8ff
Shane Satterfield <dustyplant@gmail.com>
2015-08-26 22:12:55 -0700

    Finished SQL -> Select -> Weather Observation Station 12

--- SQL/Select/weather_observation_station_12.sql
+++ SQL/Select/weather_observation_station_12.sql
@@ -11,10 +11,10 @@ Given a table STATION that holds data for five fields namely ID, CITY, STATE, NO
 | LONG_W      | NUMERIC    |
 +-------------+------------+
 
-Write a query to print the list of CITY that does not start with vowels or does not end with vowels in lexicographical order. Do not print duplicates.
+Write a query to print the list of CITY that does not start with vowels and does not end with vowels in lexicographical order. Do not print duplicates.
 */
 
 SELECT DISTINCT(CITY)
 FROM STATION
-WHERE CITY REGEXP "^[^aeiou].+" or CITY REGEXP ".+[^aeiou]$"
+WHERE CITY REGEXP "^[^aeiou].+[^aeiou]$"
 ORDER BY CITY;
