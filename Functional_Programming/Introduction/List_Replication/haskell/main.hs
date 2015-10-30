4a0816e5c9b8b0095d7a98b322869873b181f789
Shane Satterfield <dustyplant@gmail.com>
2015-10-29 22:39:44 -0700

    Finished Functional Programming -> Introduction -> List Replication

---
+++ Functional_Programming/Introduction/List_Replication/haskell/main.hs
@@ -0,0 +1,7 @@
+import Test.Hspec
+
+replicant n arr = foldl (\x y -> x ++ (replicate n y)) [] arr
+
+main :: IO ()
+main = getContents >>=
+       mapM_ print. (\(n:arr) -> replicant n arr). map read. words
